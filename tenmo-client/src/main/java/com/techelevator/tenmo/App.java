e700d6c0cd698430be95a65ccebe4137ea74d75d
PatrickETucker <petucker@outlook.com>
2022-08-20 14:59:55 -0700

    adds backend for step 3

--- tenmo-client/src/main/java/com/techelevator/tenmo/App.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/App.java
@@ -2,6 +2,7 @@ package com.techelevator.tenmo;
 
 import com.techelevator.tenmo.model.AuthenticatedUser;
 import com.techelevator.tenmo.model.UserCredentials;
+import com.techelevator.tenmo.services.AccountService;
 import com.techelevator.tenmo.services.AuthenticationService;
 import com.techelevator.tenmo.services.ConsoleService;
 
@@ -11,6 +12,7 @@ public class App {
 
     private final ConsoleService consoleService = new ConsoleService();
     private final AuthenticationService authenticationService = new AuthenticationService(API_BASE_URL);
+    private final AccountService accountService = new AccountService(API_BASE_URL);
 
     private AuthenticatedUser currentUser;
 
@@ -86,7 +88,7 @@ public class App {
 
 	private void viewCurrentBalance() {
 		// TODO Auto-generated method stub
-		
+        System.out.println("Your current account balance is: $" + accountService.getBalance(currentUser));
 	}
 
 	private void viewTransferHistory() {
--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -1,11 +1,31 @@
 package com.techelevator.tenmo.controller;
 
+import com.techelevator.tenmo.dao.AccountDao;
 import com.techelevator.tenmo.dao.JdbcAccountDao;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.math.BigDecimal;
+
+@RestController
+@PreAuthorize("isAuthenticated()")
+@RequestMapping("/account")
 public class AccountController {
 
-    private JdbcAccountDao jdbcAccountDao;
+    private AccountDao dao;
 
-    
+    public AccountController(AccountDao dao) {
+        this.dao = dao;
+    }
+
+    @GetMapping("/{id}")
+    public BigDecimal getAccount(@Valid @PathVariable Long id) {
+        return dao.getBalance(id);
+    }
+
 
 }
