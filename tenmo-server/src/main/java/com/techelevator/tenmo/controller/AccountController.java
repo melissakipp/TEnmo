44320b31c423653b61310b054a2d61689f46b88a
PatrickETucker <petucker@outlook.com>
2022-08-21 12:52:00 -0700

    changes approach for getting balance, adds /account and /account/balance endpoints

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -1,6 +1,7 @@
 package com.techelevator.tenmo.controller;
 
 import com.techelevator.tenmo.dao.AccountDao;
+import com.techelevator.tenmo.model.AccountDTO;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,13 +20,19 @@ public class AccountController {
     public AccountController(AccountDao dao) {
         this.dao = dao;
     }
-
-//    @GetMapping("/{id}")
-//    public BigDecimal getAccount(@Valid @PathVariable Long id, Principal principal) {
-//        return dao.getBalance(id, principal);
-//    }
+/* Previous implementation
+    @GetMapping("/{id}")
+    public BigDecimal getAccount(@Valid @PathVariable Long id, Principal principal) {
+        return dao.getBalance(id, principal);
+    }
+*/
 
     @GetMapping("")
+    public AccountDTO getAccount(Principal principal) {
+        return dao.getAccount(principal.getName());
+    }
+
+    @GetMapping("/balance")
     public BigDecimal getAccountBalance(Principal principal) {
         return dao.getAccountBalance(principal.getName());
     }
