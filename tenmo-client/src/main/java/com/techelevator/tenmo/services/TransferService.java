0dc76d6b62ec1f3da29e706dcdab883f39644145
Melissa Kipp <melissakipp.az@gmail.com>
2022-08-22 05:29:26 -0700

    Added Transfer Service skeleton class

---
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
@@ -0,0 +1,10 @@
+package com.techelevator.tenmo.services;
+
+import java.math.BigDecimal;
+
+public class TransferService  {
+
+    public BigDecimal transfer(Long accountFrom, Long accountTo, BigDecimal amount) {
+        return null;
+    }
+}
