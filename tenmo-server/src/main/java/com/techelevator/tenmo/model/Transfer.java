af82f5eaaf77c51921604b2375f12298699bb960
Melissa Kipp <melissakipp.az@gmail.com>
2022-08-22 04:53:58 -0700

    Added transfter class

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/Transfer.java
@@ -0,0 +1,102 @@
+package com.techelevator.tenmo.model;
+
+import java.math.BigDecimal;
+
+public class Transfer {
+
+    private Long transferId;
+    private Long transferTypeId;
+    private String transferTypeDescription;
+    private Long transferStatusId;
+    private String transferStatusDescription;
+    private Long accountFrom;
+    private Long accountTo;
+    private BigDecimal amount;
+
+
+
+    public Transfer(
+            Long transferId,
+            Long transferTypeId,
+            String transferTypeDescription,
+            Long transferStatusId,
+            String transferStatusDescription,
+            Long accountFrom,
+            Long accountTo,
+            BigDecimal amount ) {
+        this.transferId = transferId;
+        this.transferTypeId = transferTypeId;
+        this.transferTypeDescription = transferTypeDescription;
+        this.transferStatusId = transferStatusId;
+        this.transferStatusDescription = transferStatusDescription;
+        this.accountFrom = accountFrom;
+        this.accountTo = accountTo;
+        this.amount = amount;
+    }
+
+    public Transfer(){}
+
+    public Long getTransferId() {
+        return transferId;
+    }
+
+    public void setTransferId(Long transferId) {
+        this.transferId = transferId;
+    }
+
+    public Long getTransferTypeId() {
+        return transferTypeId;
+    }
+
+    public void setTransferTypeId(Long transferTypeId) {
+        this.transferTypeId = transferTypeId;
+    }
+    public String getTransferTypeDescription() {
+        return transferTypeDescription;
+    }
+
+    public void setTransferTypeDescription(String transferTypeDescription) {
+        this.transferTypeDescription = transferTypeDescription;
+    }
+
+    public Long getTransferStatusId() {
+        return transferStatusId;
+    }
+
+    public void setTransferStatusId(Long transferStatusId) {
+        this.transferStatusId = transferStatusId;
+    }
+
+    public String getTransferStatusDescription() {
+        return transferStatusDescription;
+    }
+
+    public void setTransferStatusDescription(String transferStatusDescription) {
+        this.transferStatusDescription = transferStatusDescription;
+    }
+
+    public Long getAccountFrom() {
+        return accountFrom;
+    }
+
+    public void setAccountFrom(Long accountFrom) {
+        this.accountFrom = accountFrom;
+    }
+
+    public Long getAccountTo() {
+        return accountTo;
+    }
+
+    public void setAccountTo(Long accountTo) {
+        this.accountTo = accountTo;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+}
